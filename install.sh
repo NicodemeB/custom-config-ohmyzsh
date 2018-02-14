7182f625c0afb74b3cbcedfb926c708ad1082377
benjamin <benjamin.nicodeme@icloud.com>
2018-02-14 13:29:50 +0100

    cc 1.0 update

--- install.sh
+++ install.sh
@@ -12,8 +12,11 @@ rm -rf fonts
 
 
 sed -i 's/robbyrussell/agnoster/' ~/.zshrc
-sed -i 's/plugins=(git)/plugins=(\n\tgit\n\tnanoc\n\tz\n\t)/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(\n\tgit\n\tnanoc\n\tz\n\tzsh-autosuggestions\n\t)/' ~/.zshrc
 sed -i "s/prompt_segment blue black '%~'/prompt_segment blue black '%c'/" ~/.oh-my-zsh/themes/agnoster.zsh-theme
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 
 source ~/.zshrc
 
