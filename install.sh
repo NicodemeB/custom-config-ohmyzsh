c99e1d6a0b288f6fe148a6ef9129e11b645df231
benjamin <benjamin.nicodeme@icloud.com>
2018-03-31 19:11:09 +0200

    edites

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
-#!/usr/bin/env zsh
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
+
 
 sudo apt install git zsh
 
+printf "${YELLOW}[INFO] - PLEASE TYPE 'exit' AFTER OHMYZSH HAS FINISHED TO INSTALL TO FINISH THE INSTALL ${NORMAL}\n" 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -20,7 +28,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 cd .. 
 rm -rf custom-config-ohmyzsh
 
-source ~/.zshrc
+zsh -c "source ~/.zshrc"
+zsh
 
 
 
